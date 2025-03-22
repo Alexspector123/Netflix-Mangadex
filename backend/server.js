c822795202d6452ebeac8c055de73b51c403b885
FanchonSora <khanhngan1491@gmail.com>
2025-03-22 14:49:28 +0700

    done backend, didn't check bug

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,24 @@ import express from 'express';
 import authRoutes from './routes/auth.route.js';
 import movieRoutes from './routes/movie.route.js';
 import tvRoutes from './routes/tv.route.js';
+import searchRoutes from './routes/search.route.js';
 
+import { protectRoute } from './middleware/protectRoute.js';
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
+
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = ENV_VARS.PORT;
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
-app.use("/api/v1/tv", tvRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(PORT, () => {
     console.log('server running at http://localhost:' + PORT);
