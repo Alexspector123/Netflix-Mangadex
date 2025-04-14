b99831cdb3cc52ce236cf2c451b4bb4cc5a6a853
FanchonSora <khanhngan1491@gmail.com>
2025-04-14 16:13:40 +0700

    chinrh xiu cu fix tiep di

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -2,7 +2,7 @@ import { fetchFromTMDB } from "../services/tmdb.service.js";
 
 export async function getTrendingMovies(req, res) {
     try {
-        const data = await fetchFromTMDB("https://api.themoviedb.org/3/trending/movie/day?language=en-US");
+        const data = await fetchFromTMDB('https://api.themoviedb.org/3/trending/movie/day?language=en-US');
         const randomMovie = data.results[Math.floor(Math.random() * data.results?.length)];
 
         res.json({success: true, content: randomMovie});
@@ -54,13 +54,14 @@ export async function getSimilarMovies(req, res) {
 }
 
 export async function getMoviesByCategory(req, res) {
-    const category = req.params;
+    const { category } = req.params;
     try {
-        const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${category}?language=en-US&page=1`);
-
-        res.status(200).json({success: true, content: data.results});
-
+        const data = await fetchFromTMDB(
+          `https://api.themoviedb.org/3/movie/${category}?language=en-US&page=1`
+        );
+        res.status(200).json({ success: true, content: data.results });
     } catch (error) {
-        res.status(500).json({success: false, message: "Internal Server Error"});
+        console.error(error);
+        res.status(500).json({ success: false, message: "Internal Server Error" });
     }
 }
--- backend/services/tmdb.service.js
+++ backend/services/tmdb.service.js
@@ -2,16 +2,23 @@ import axios from "axios";
 import { ENV_VARS } from "../config/envVars.js";
 
 export const fetchFromTMDB = async (url) => {
-    const options = {
-        headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer ' + ENV_VARS.TMDB_API_KEY
-        }
-    };
+  const options = {
+    method: 'GET',
+    headers: {
+        accept: "application/json",
+        Authorization: "Bearer " + ENV_VARS.TMDB_API_KEY,
+    },
+  };
 
+  try {
     const response = await axios.get(url, options);
-    if(response.status !== 200) {
-        throw new Error('Failed to fetch data from TMDB' + response.statusText);
+    if (response.status !== 200) {
+      throw new Error("Failed to fetch data from TMDB: " + response.statusText);
     }
+    console.log("Connect successfully to TMDB API:", url);
     return response.data;
-}
+  } catch (error) {
+    console.error("Error fetching from TMDB:", error.message);
+    throw error;
+  }
+};
