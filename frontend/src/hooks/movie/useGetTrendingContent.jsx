862dcd4e0f0326235a6219970383f77a7ac9c983
Phan Tran Thanh Huy <alexspector8766@gmail.com>
2025-05-06 10:12:13 +0700

    Chore (Language)

--- frontend/src/hooks/movie/useGetTrendingContent.jsx
+++ frontend/src/hooks/movie/useGetTrendingContent.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useContentStore } from "../store/content.js";
+import { useContentStore } from "../../store/content.js";
 import axios from "axios";
 
 const useGetTrendingContent = () => {
